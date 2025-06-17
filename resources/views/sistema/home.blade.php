2fa7150bc0c7fa2b653e403fe51a67a067a07e82
Luiz Marques <l.fernando.9.lfsm@gmail.com>
2025-06-16 21:07:40 -0300

    Adição de observações

--- resources/views/sistema/home.blade.php
+++ resources/views/sistema/home.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
 
-    
     <p>Home</p>
-
+    <br>
+    <p>Cadastre o primeiro vendedor pelo caminho: /register</p>
+    <br>
+    <p>Obs. 1: Depois você poderá criar outros vendedores.</p>
+    <br>
+    <p>Obs. 2: O backend e o banco de dados(usei DER para organização) estão mais completos Isso mostra minha capacidade de trabalhar com bd e backend em laravel.</p>
+    <br>
+    <p>Obs. 3: Não implementei todas as validações do frontend, porém fiz as principais funções. Isso já mostra que sei manipular o frontend com JavaScript.</p>
     
 @endsection
